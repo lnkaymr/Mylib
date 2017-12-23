7303365aab99f46edb0deb1e1c0312bf87d8efc2
Lnkaymr <lnkaymr@outlook.com>
2017-12-23 21:32:59 +0800

    My lib

---
+++ 电阻电容电感.d
@@ -0,0 +1,30 @@
+*PADS-LIBRARY-PCB-DECALS-V9*
+
+QD-C0402-20-38   I 0     0     0 2 2 0 2 1 0
+TIMESTAMP 2017.09.14.04.11.45
+-21   10    0     0 20    2     10 32 33 "Regular <Romansim Stroke Font>"
+Value
+-29   21    0     0 30    4     26 0 34 "Regular <Romansim Stroke Font>"
+REF-DES
+OPEN   6 4 26 -1
+-13   14   
+-31   14   
+-33   12   
+-33   -12  
+-31   -14  
+-13   -14  
+OPEN   6 4 26 -1
+13    14   
+31    14   
+33    12   
+33    -12  
+31    -14  
+13    -14  
+T-19   0     -19   0     1
+T19    0     19    0     2
+PAD 0 3 P 0    
+-2 20  S   0  
+-1 0   S   0  
+0  0   S   0  
+
+*END*
